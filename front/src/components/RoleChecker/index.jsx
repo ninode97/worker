fb1e80444a46acace7c38c53f70a1e01899e952b
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 20:29:43 +0300

    blank

--- front/src/components/RoleChecker/index.jsx
+++ front/src/components/RoleChecker/index.jsx
@@ -4,25 +4,7 @@ import MainContainer from '../MainContainer';
 
 const RoleChecker = props => {
   console.log(props.authReducer.user.role);
-  return (
-    <MainContainer>
-      <div className='admin-menu'>
-        <span className='login100-form-title p-b-26'>App Menu</span>
-        <button className='login100-form-btn'>Login</button>
-
-        <div className='text-center p-t-115'>
-          <span className='txt1'>Found a problem?</span>
-          <a
-            style={{ color: 'dodgerblue', paddingLeft: '5px' }}
-            className='txt2'
-            href='/contact'
-          >
-            Report
-          </a>
-        </div>
-      </div>
-    </MainContainer>
-  );
+  return <h1>Hello WOrld!</h1>;
 };
 
 const mapStateToProps = store => ({
