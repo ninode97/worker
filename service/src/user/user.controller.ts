f826a1495b8b740f25c127de9eb04a7ce7d8bf0e
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 17:18:15 +0300

    blank

--- service/src/user/user.controller.ts
+++ service/src/user/user.controller.ts
@@ -3,9 +3,10 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 
 @Controller('user')
+@UseGuards(AuthGuard())
 export class UserController {
   @Get('/')
   getMe(@Req() request: Request) {
-    return request;
+    return 'ook';
   }
 }
