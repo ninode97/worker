d9424f5a7aaec36e623d5c49e9e22e910062db19
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 12:04:52 +0300

    Defined JWT strategy, created custom GetUser param

--- service/src/auth/auth.controller.ts
+++ service/src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/user/decorators/get-user.decorator';
+import { User } from 'src/user/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +31,11 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     console.log(`LOGIN request received: ${loginUserDto}`);
     return this.authService.signIn(loginUserDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@GetUser() user: User) {
+    console.log(user);
   }
 }
