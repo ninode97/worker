41ee8bc373a6502d52b0e2aadbc0253a46331f2f
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 20:34:31 +0300

    blank

--- front/src/components/AdminView/index.jsx
+++ front/src/components/AdminView/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import MainContainer from '../MainContainer';
 
 const AdminView = props => {
   console.log(props.authReducer.user.role);
