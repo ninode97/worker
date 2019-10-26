7a51a0513ef1e1b8da35d10ba9bee8eef5bd9b4d
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 11:06:07 +0300

    Added AUth service to theauth controlelr

--- service/src/auth/auth.controller.ts
+++ service/src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
   @Post('/login')
   login(@Body() loginUserDto: LoginUserDto) {
     console.log(loginUserDto);
