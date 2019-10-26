c4882047f714a072f7d200a152f97bc15146a6dd
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 19:08:40 +0300

    blank

--- front/src/components/LoginForm.jsx
+++ front/src/components/LoginForm.jsx
@@ -11,7 +11,8 @@ const LoginForm = props => {
   const signInHandler = async e => {
     e.preventDefault();
     props.login({ username, password }).then(
-      res => window.location.reload(),
+      console.log(username),
+      // res => window.location.reload(),
       err => {
         console.log(err);
         setError({ error: 'saddly' });
