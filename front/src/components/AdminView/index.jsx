1fcdbd5c669768dc10395380866fe9b046cbc734
Pabijonavicius <lukas.pabijonavicius@gmail.com>
2019-10-26 20:36:22 +0300

    blank

--- front/src/components/AdminView/index.jsx
+++ front/src/components/AdminView/index.jsx
@@ -8,18 +8,20 @@ const AdminView = props => {
     <MainContainer>
       <div className='admin-menu'>
         <span className='login100-form-title p-b-26'>Admin Menu</span>
-        <button className='login100-form-btn'>Login</button>
+        <form className='login100-form validate-form'>
+          <button className='login100-form-btn'>Login</button>
 
-        <div className='text-center p-t-115'>
-          <span className='txt1'>Found a problem?</span>
-          <a
-            style={{ color: 'dodgerblue', paddingLeft: '5px' }}
-            className='txt2'
-            href='/contact'
-          >
-            Report
-          </a>
-        </div>
+          <div className='text-center p-t-115'>
+            <span className='txt1'>Found a problem?</span>
+            <a
+              style={{ color: 'dodgerblue', paddingLeft: '5px' }}
+              className='txt2'
+              href='/contact'
+            >
+              Report
+            </a>
+          </div>
+        </form>
       </div>
     </MainContainer>
   );
