7e1370c485cc827fe890d112637d0fb395737d01
sphere <sphere@localhost.localdomain>
2019-10-30 10:15:29 -0400

    blank

--- front/src/components/AdminView/AdminWorkers/index.jsx
+++ front/src/components/AdminView/AdminWorkers/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const AdminWorkers = () => {
-  return <div>Helllo Workers!</div>;
+  return (
+    <div>
+      <button style={styles.button}>Add Worker</button>
+      <button style={styles.button}>Update Worker</button>
+      <button style={styles.button}>Delete Worker</button>
+      <button style={styles.button}>Find Worker</button>
+    </div>
+  );
+};
+
+const styles = {
+  button: {
+    background: 'gainsboro',
+    width: '100%',
+    padding: '1rem',
+    border: '1px solid white',
+    '&:hover': {
+      background: 'red'
+    }
+  }
 };
 
 export default AdminWorkers;
