fd2b1e28ddfbe8eb397b5f21d9c9911170faeaf0
sphere <sphere@localhost.localdomain>
2019-10-31 12:59:46 -0400

    blank

--- front/src/actions/adminMenuActions.js
+++ front/src/actions/adminMenuActions.js
@@ -1,18 +1,16 @@
 import { ADD_USER, DELETE_USER, UPDATE_USER, FIND_USER } from './types';
 import axios from 'axios';
 
-export async function addUser(user) {
-  const response = await axios
+export function addUser(user) {
+  axios
     .post('/api/auth/signup', user)
     .then(res => {
-      console.log(res);
-      return res;
+      return {
+        type: ADD_USER,
+        payload: res
+      };
     })
     .catch(err => err.response);
-  return {
-    type: ADD_USER,
-    payload: response
-  };
 }
 export function deleteUser(error) {
   return {
