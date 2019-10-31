5566bddb7fedd2dc25c4f8f63da000d1e6732a59
sphere <sphere@localhost.localdomain>
2019-10-31 13:18:52 -0400

    blank

--- front/src/components/AdminView/AdminWorkersUpdate/index.jsx
+++ front/src/components/AdminView/AdminWorkersUpdate/index.jsx
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Title from '../../shared/Title';
+import InputControl from '../../shared/InputControl';
+import Input from '../../shared/Input';
+import InputPlaceholder from '../../shared/InputPlaceholder';
+import FormButton from '../../shared/buttons/formButton/FormButton';
+import FormButtonContainer from '../../shared/buttons/formButton/FormButtonContainer';
+import FormButtonWrapper from '../../shared/buttons/formButton/FormButtonWrapper';
+import { formatMessage } from '../../../utils/utils';
+
 const AdminWorkersUpdate = () => {
-  return <h1>UPDATE</h1>;
+  const [message, setMessage] = useState(null);
+  const [username, setUsername] = useState('');
+
+  function findUser() {}
+
+  return (
+    <div style={styles.container}>
+      {formatMessage(message)}
+      <Title title="Add Worker" />
+      <form onSubmit={findUser} style={styles.form}>
+        <InputControl>
+          <Input
+            onChange={e => setUsername(e.target.value)}
+            type="text"
+            name="username"
+            id="username"
+          />
+          <InputPlaceholder placeholder="Username" />
+        </InputControl>
+        <FormButtonContainer>
+          <FormButtonWrapper>
+            <FormButton>Add User</FormButton>
+          </FormButtonWrapper>
+        </FormButtonContainer>
+      </form>
+    </div>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  form: {
+    padding: '1rem 4rem'
+  }
 };
 
 export default AdminWorkersUpdate;
