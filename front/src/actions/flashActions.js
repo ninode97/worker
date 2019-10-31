de15329785e9cc09ce330c6df7648e1adaae7826
sphere <sphere@localhost.localdomain>
2019-10-31 11:28:14 -0400

    blank

--- front/src/actions/flashActions.js
+++ front/src/actions/flashActions.js
@@ -2,7 +2,7 @@ import { SET_MESSAGE, REMOVE_MESSAEGE, SHOW_MESSAGE } from './types';
 
 export function setMessage(flashMessage) {
   return {
-    type: SET_MESSAGE,
+    type: 'SET_MESSAGE',
     flashMessage
   };
 }
