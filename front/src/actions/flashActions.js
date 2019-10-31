da5b41907cfeb81c5973c3258e6f8fc5701b2e35
sphere <sphere@localhost.localdomain>
2019-10-31 11:28:58 -0400

    blank

--- front/src/actions/flashActions.js
+++ front/src/actions/flashActions.js
@@ -1,6 +1,7 @@
 import { SET_MESSAGE, REMOVE_MESSAEGE, SHOW_MESSAGE } from './types';
 
 export function setMessage(flashMessage) {
+  alert('VEIKIA');
   return {
     type: 'SET_MESSAGE',
     flashMessage
